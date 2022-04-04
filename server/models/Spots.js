289fe974e6ddf198ac6f01e13fc24b47f2eb9df6
aaronespinoza <aarone@berkeley.edu>
2022-04-03 19:12:51 -0700

    added dateFormat util which the spots model uses for created at property

--- server/models/Spots.js
+++ server/models/Spots.js
@@ -1,4 +1,5 @@
 const { Schema } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const spotsSchema = new Schema({
@@ -23,6 +24,11 @@ const spotsSchema = new Schema({
   title: {
     type: String,
     required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
   },
 });
 
